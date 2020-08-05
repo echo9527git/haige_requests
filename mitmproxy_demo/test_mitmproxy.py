0e990d5687ca3223f2101cf9c0fb3bcc1fddf8cb
echo9527git <echooche@126.com>
2020-08-05 13:33:02 +0800

    request部分api：post-->form、json、xml、headers

--- mitmproxy_demo/test_mitmproxy.py
+++ mitmproxy_demo/test_mitmproxy.py
@@ -1,15 +1,22 @@
-from mitmproxy import ctx
+def request(flow):
+    flow.request.headers["myheader"] = "value"
 
+"""Send a reply from the proxy without sending any data to the remote server."""
+from mitmproxy import http
 
-class Counter:
-    def __init__(self):
-        self.num = 0
+# def request(flow: http.HTTPFlow) -> None:
+#     if flow.request.pretty_url == "http://www.baidu.com/":
+#         flow.response = http.HTTPResponse.make(
+#             200,  # (optional) status code
+#             b"Hello World12345",  # (optional) content
+#             {"Content-Type": "text/html"}  # (optional) headers
+#         )
 
-    def request(self, flow):
-        self.num = self.num + 1
-        ctx.log.info("We've seen %d flows" % self.num)
-
-
-addons = [
-    Counter()
-]
+def request(flow: http.HTTPFlow) -> None:
+    if "get_token=abcdef" in flow.request.pretty_url:
+        with open("C:/Users/Administrator/Desktop/map1.json",encoding="UTF-8") as f:
+            flow.response = http.HTTPResponse.make(
+                200,  # (optional) status code
+                f.read(),
+                {"Content-Type": "text/html"}  # (optional) headers
+            )
---
+++ requests_hog/test_hog1.py
@@ -0,0 +1,60 @@
+import requests
+
+from requests_demo0730 import utils
+from jsonpath import jsonpath
+
+
+class TestHog1:
+    def test_get(self):
+        r = requests.get("http://httpbin.testing-studio.com/get")
+        print(r.text)
+        assert r.status_code ==200
+
+    def test_query(self):
+        payload = {
+            "level":1,
+            "name":'echo'
+        }
+        r = requests.get("http://httpbin.testing-studio.com/get",params=payload)
+        print(r.text)
+        assert r.status_code ==200
+
+    def test_heaaders(self):
+        header = {
+            "h": 'header demo'
+        }
+        r = requests.get("http://httpbin.testing-studio.com/get", headers=header)
+        print(r.text)
+        assert r.status_code == 200
+        assert r.json()['headers']['H'] == "header demo"
+
+    def test_post_form(self):
+        payload = {
+            "level": 1,
+            "name": 'echo'
+        }
+        r = requests.post("http://httpbin.testing-studio.com/post", data=payload)
+        print(r.text)
+        assert r.status_code == 200
+
+    # "Content-Type": "application/json"
+    def test_post_json(self):
+        payload = {
+            "level": 1,
+            "name": 'echo'
+        }
+        r = requests.post("http://httpbin.testing-studio.com/post", json=payload)
+        print(r.text)
+        assert r.status_code == 200
+        assert r.json()['json']['level'] == 1
+
+    # requests没有提供对xml封装的接口，所有使用data（文本）来发送数据，同时需要修改headers中的Content-Type
+    # "Content-Type": "application/xml"
+    def test_post_xml(self):
+        pass
+
+    def test_hog_json(self):
+        r = requests.get('https://ceshiren.com/categories.json')
+        utils.print_json(r)
+        assert r.json()['category_list']['categories'][1]['name'] == '开源项目'
+        assert jsonpath(r.json(),'$..name')[1] == '开源项目'
